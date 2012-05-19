be659d9ffd1042e6029d6122b1282745a4f51d9e
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-05-18 19:46:06 -0500

    cleaned up setup.py filew

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
-import os
-
 from setuptools import setup, find_packages
 
 
 setup(name='moz_inapp_pay',
-      version='1.0',
+      version='1.0.1',
       description='Utility for working with Mozilla in-app payments.',
       long_description='',
-      author='',
+      author='Kumar McMillan',
       author_email='kumar.mcmillan@gmail.com',
       license='BSD',
-      url='',
+      url='https://github.com/kumar303/moz_inapp_pay',
       include_package_data=True,
-      classifiers = [],
+      classifiers=[],
       packages=find_packages(exclude=['tests']),
       install_requires=['PyJWT',
                         'M2Crypto>=0.2.0'])
