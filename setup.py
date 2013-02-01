3ac64c0172a03df5bd086faa99090ce877331b2f
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-01-31 17:52:50 -0800

    Bump for PyPI release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='moz_inapp_pay',
-      version='1.0.3',
+      version='1.0.4',
       description='Utility for working with Mozilla in-app payments.',
       long_description='',
       author='Kumar McMillan',
